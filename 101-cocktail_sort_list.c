f4f2160865dc0f246dc7e56b87f61931c39951bb
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-02-20 23:02:32 +0100

    Bitonic not yet okay

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,95 +1,69 @@
-#include <stdio.h>
 #include "sort.h"
 
 /**
  * swap_nodes - Swaps two nodes in a doubly linked list
- * @list: Pointer to the head of the list
- * @left: Pointer to the left node to swap
- * @right: Pointer to the right node to swap
+ * @a: First node to swap
+ * @b: Second node to swap
  */
-void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
+void swap_nodes(listint_t **a, listint_t **b)
 {
-	if (left->prev != NULL)
-		left->prev->next = right;
-	else
-		*list = right;
+	listint_t *temp = *a;
 
-	if (right->next != NULL)
-		right->next->prev = left;
+	temp->next = (*b)->next;
+	if (temp->next != NULL)
+		temp->next->prev = temp;
+	(*b)->next = temp;
 
-	left->next = right->next;
-	right->prev = left->prev;
-	right->next = left;
-	left->prev = right;
+	temp->prev = (*b)->prev;
+	if (temp->prev != NULL)
+		temp->prev->next = temp;
+	(*b)->prev = temp;
 
-	print_list(*list);
+	if (temp->prev == NULL)
+		*a = *b;
+
+	*b = temp;
 }
 
 /**
- * forward_pass - Performs a forward pass of the Cocktail Shaker Sort algorithm
- * @list: Pointer to the pointer to the head of the list
- * @swapped: Pointer to the variable indicating whether any swaps were made
- */
-void forward_pass(listint_t **list, int *swapped)
-{
-	listint_t *temp = *list;
-
-	while (temp != NULL && temp->next != NULL)
-	{
-		if (temp->n > temp->next->n)
-		{
-			swap_nodes(list, temp, temp->next);
-			*swapped = 1;
-		}
-		else
-		{
-			temp = temp->next;
-		}
-	}
-}
-
-/**
- * backward_pass - Performs a backward pass of the Cocktail Shaker
- * 		Sort algorithm
- * @list: Pointer to the pointer to the head of the list
- * @swapped: Pointer to the variable indicating whether any swaps were made
- */
-void backward_pass(listint_t **list, int *swapped)
-{
-	listint_t *temp = *list;
-
-	while (temp != NULL && temp->prev != NULL)
-	{
-		if (temp->prev->n > temp->n)
-		{
-			swap_nodes(list, temp->prev, temp);
-			*swapped = 1;
-		}
-		else
-		{
-			temp = temp->prev;
-		}
-	}
-}
-
-/**
- * cocktail_sort_list - Sorts a doubly linked list of integers in ascending
- *                      order using the Cocktail Shaker Sort algorithm
- * @list: Pointer to the pointer to the head of the list
+ * cocktail_sort_list - Sorts a doubly linked list in ascending order
+ *                      using Cocktail Shaker sort algorithm
+ * @list: Pointer to the head of the doubly linked list
  */
 void cocktail_sort_list(listint_t **list)
 {
-	int swapped = 1;
+	int swapped;
+	listint_t *temp;
 
 	if (list == NULL || *list == NULL)
 		return;
 
-	while (swapped)
-	{
+	do {
 		swapped = 0;
-		forward_pass(list, &swapped);
+
+		for (temp = *list; temp->next != NULL; temp = temp->next)
+		{
+			if (temp->n > temp->next->n)
+			{
+				swap_nodes(&temp, &(temp->next));
+				swapped = 1;
+				print_list(*list);
+			}
+		}
+
 		if (!swapped)
 			break;
-		backward_pass(list, &swapped);
-	}
+
+		swapped = 0;
+
+		for (; temp->prev != NULL; temp = temp->prev)
+		{
+			if (temp->n > temp->prev->n)
+			{
+				swap_nodes(&temp, &(temp->prev));
+				swapped = 1;
+				print_list(*list);
+			}
+		}
+	} while (swapped);
 }
--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,6 +1,95 @@
 #include "sort.h"
 #include <stdio.h>
 
+
+void bitonic_sort_recursive(
+		int *array,
+		size_t size,
+		int dir,
+		size_t bitonic_size
+		);
+
+void bitonic_merge(int *array, size_t size, int dir, size_t bitonic_size);
+void swap(int *a, int *b);
+
+/**
+ * bitonic_sort - Sorts an array of integers in ascending
+ *		order using Bitonic sort
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ */
+void bitonic_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	printf("Merging [%lu/%lu] (UP):\n", size, size);
+	print_array(array, size);
+
+	bitonic_sort_recursive(array, size, 1, size);
+
+	printf("\n");
+}
+
+/**
+ * bitonic_sort_recursive - Recursive function to perform Bitonic sort
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ * @dir: Sorting direction (1 for ascending, 0 for descending)
+ * @bitonic_size: Size of the subsequence to be sorted
+ */
+void bitonic_sort_recursive(
+		int *array,
+		size_t size,
+		int dir,
+		size_t bitonic_size
+		)
+{
+	if (bitonic_size > 1)
+	{
+		size_t half = bitonic_size / 2;
+
+		bitonic_sort_recursive(array, half, 1, half);
+		bitonic_sort_recursive(array + half, half, 0, half);
+
+		printf("Merging [%lu/%lu] (%s):\n", size, bitonic_size, dir ? "UP" : "DOWN");
+		print_array(array, size);
+
+		bitonic_merge(array, size, dir, bitonic_size);
+	}
+}
+
+/**
+ * bitonic_merge - Merges two sorted subsequences in Bitonic manner
+ * @array: The array containing the subsequences to be merged
+ * @size: Number of elements in the array
+ * @dir: Sorting direction (1 for ascending, 0 for descending)
+ * @bitonic_size: Size of the subsequences to be merged
+ */
+
+void bitonic_merge(int *array, size_t size, int dir, size_t bitonic_size)
+{
+	if (bitonic_size > 1)
+	{
+		size_t half = bitonic_size / 2;
+		size_t i;
+
+		for (i = 0; i + half < size; ++i)
+		{
+			int j = i + half;
+
+			if (dir ? array[i] > array[j] : array[i] < array[j])
+				swap(&array[i], &array[j]);
+		}
+
+		printf("Result [%lu/%lu] (%s):\n", half, bitonic_size, dir ? "UP" : "DOWN");
+		print_array(array, size);
+
+		bitonic_merge(array, half, dir, half);
+		bitonic_merge(array + half, half, dir, half);
+	}
+}
+
 /**
  * swap - Swaps two elements in an array
  * @a: Pointer to the first element
@@ -11,56 +100,4 @@ void swap(int *a, int *b)
 	int temp = *a;
 	*a = *b;
 	*b = temp;
-}
-
-/**
- * bitonic_merge - Merges two subarrays in a bitonic manner
- * @array: Array to be sorted
- * @start: Starting index of the first subarray
- * @count: Number of elements to be merged
- * @dir: Direction of sorting (1 for ascending, 0 for descending)
- */
-void bitonic_merge(int *array, int start, int count, int dir)
-{
-	if (count > 1)
-	{
-		int i, k = count / 2;
-		for (i = start; i < start + k; i++)
-		{
-			if ((array[i] > array[i + k]) == dir)
-				swap(&array[i], &array[i + k]);
-		}
-		bitonic_merge(array, start, k, dir);
-		bitonic_merge(array, start + k, k, dir);
-	}
-}
-
-/**
- * bitonic_sort_recursive - Recursively performs bitonic sort
- * @array: Array to be sorted
- * @start: Starting index of the subarray
- * @count: Number of elements to be sorted
- * @dir: Direction of sorting (1 for ascending, 0 for descending)
- */
-void bitonic_sort_recursive(int *array, int start, int count, int dir)
-{
-	int k;
-	if (count > 1)
-	{
-		k = count / 2;
-		bitonic_sort_recursive(array, start, k, 1);
-		bitonic_sort_recursive(array, start + k, k, 0);
-		bitonic_merge(array, start, count, dir);
-	}
-}
-
-/**
- * bitonic_sort - Sorts an array of integers in ascending
- * 		order using Bitonic sort
- * @array: Array to be sorted
- * @size: Number of elements in the array
- */
-void bitonic_sort(int *array, size_t size)
-{
-	bitonic_sort_recursive(array, 0, size, 1);
 }
